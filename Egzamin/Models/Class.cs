19e8ec94983d41f6356f414005cd2c55065f6451
ignac <ignac@CBDB>
2022-02-23 10:48:53 +0100

    Dodaj pliki projektów.

---
+++ Egzamin/Models/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Egzamin.Models
+{
+    public class Class
+    {
+        public int IdZad { get; set; }
+        public string Nazwa { get; set; }
+        public string  Opis { get; set; }
+        public bool Zrob { get; set; }
+    }
+}
